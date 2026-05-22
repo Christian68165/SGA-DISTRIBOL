2b4ffe6c2a36fcb94c34ac905ab576e4fbec3295
Christian <josechristianvargascondori@gmail.com>
2026-05-22 19:04:10 -0400

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,8 +1,8 @@
 // supabase-config.js
 // Reemplaza estos valores con los de tu proyecto en Supabase
 
-const SUPABASE_URL = 'https://TU_PROYECTO.supabase.co';
-const SUPABASE_ANON_KEY = 'TU_ANON_KEY';
+const SUPABASE_URL = 'https://hygdfzpgyoqagxkmmdte.supabase.co/rest/v1/';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5Z2RmenBneW9xYWd4a21tZHRlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3OTQ3MjIwMSwiZXhwIjoyMDk1MDQ4MjAxfQ.M1r6B0rFd26KTnYRg8BqX21-bg3LhNza5GYGO-ZrmIU';
 
 // Inicializar cliente de Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
